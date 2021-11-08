deee0d69caf54e29b444627b2a0d029efdcb8fdf
AmrDeveloper <amr96@programmer.net>
2021-11-08 14:15:09 +0200

    Implement monkey lexer with test cases

---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+
+	// identifiers + literals
+	IDENT     = "IDENT"
+	INT       = "INT"
+
+	// Operators
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
+
+	GT        = ">"
+	LT        = "<"
+	EQ        = "=="
+	NOT_EQ	  = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// keywords
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
+)
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType {
+	"fn"  	 : FUNCTION,
+	"let" 	 : LET,
+	"true" 	 : TRUE,
+	"false"  : FALSE,
+	"if" 	 : IF,
+	"else" 	 : ELSE,
+	"return" : RETURN,
+}
+
+func LookupIdentifier(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
