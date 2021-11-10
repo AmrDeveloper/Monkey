664d30ec798f19cfd309f57763bbd49bf68764d0
AmrDeveloper <amr96@programmer.net>
2021-11-10 11:12:14 +0200

    Implement parsing function literal expression with tests

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 	"monkey/token"
 )
 
@@ -252,6 +253,37 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token 			token.Token
+	Parameters 		[]*Identifier
+	Body			*BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {
+
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string { 
+	return fl.Token.Literal 
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -262,6 +263,52 @@ func (p *Parser) parseIfExpression() ast.Expression {
 	}
 
 	return expression
+}
+
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral {Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return identifers
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier {Token: p.curToken, Value: p.curToken.Literal}
+	identifers = append(identifers, ident)
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+
+		ident := &ast.Identifier {Token: p.curToken, Value: p.curToken.Literal}
+		identifers = append(identifers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return identifers
 }
 
 func (p *Parser) parseBlockStatement() *ast.BlockStatement {
