637230ce99891ac0bcf97c756409aa5232cc5131
AmrDeveloper <amr96@programmer.net>
2021-11-23 13:00:54 +0200

    Implement hash

--- vm/vm.go
+++ vm/vm.go
@@ -123,6 +123,22 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpHash:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			hash, err := vm.buildHash(vm.sp - numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+
+			vm.sp = vm.sp - numElements
+
+			err = vm.push(hash)
+
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -136,6 +152,26 @@ func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
 	}
 
 	return &object.Array {Elements: elements}
+}
+
+func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
+	hashedPairs := make(map[object.HashKey]object.HashPair)
+
+	for i := startIndex; i < endIndex; i += 2 {
+		key := vm.stack[i]
+		value := vm.stack[i + 1]
+
+		pair := object.HashPair{Key: key, Value: value}
+
+		hashkey, ok := key.(object.Hashable)
+		if !ok {
+			return nil, fmt.Errorf("unusable as hash key: %s", key.Type())
+		}
+
+		hashedPairs[hashkey.HashKey()] = pair
+	}
+
+	return &object.Hash {Pairs: hashedPairs}, nil
 }
 
 func (vm *VM) executeBinaryOperation(op code.Opcode) error {
